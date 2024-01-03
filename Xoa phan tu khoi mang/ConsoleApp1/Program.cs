3396e626c09acc788abcd11b2f21400b123b36f7
Dattobias <152405844+datto2530@users.noreply.github.com>
2024-01-03 16:00:05 +0700

    da xong

---
+++ Xoa phan tu khoi mang/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("nhap gia tri muon xoa");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n; i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            Array.Resize(ref array, array.Length - 1);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+}
